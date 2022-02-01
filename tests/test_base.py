f7520a5a5a48f1b12e06d8df0e7ec19126cf643f
Salaah Amin <salaah.amin@gmail.com>
2022-02-01 01:07:32 +0000

    base unittests.

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,9 +3,8 @@
 from unittest import TestCase
 from types import SimpleNamespace
 import pandas as pd
-from data_cleaners import base
+from data_cleaners import base, exceptions
 from tests.utils import strategies
-
 
 class TestCleaningStrategy(TestCase):
     """Unittests for the `CleaningStrategy` class."""
@@ -16,12 +15,13 @@ class TestCleaningStrategy(TestCase):
         """
 
         instance = SimpleNamespace(
-            requires_django=False,
             dataframe=None,
-            model=SimpleNamespace(DataCleaner=1)
+            required_options=['option_1'],
+            option_1=1,
         )
         self.assertEqual(
-            base.CleaningStrategy.can_use_cleaner(instance), (True, None)
+            base.CleaningStrategy.can_use_cleaner(instance),
+            (True, [])
         )
 
     def test_can_use_cleaner_fail(self):
@@ -30,39 +30,36 @@ class TestCleaningStrategy(TestCase):
         """
 
         instance = SimpleNamespace(
-            requires_django=False,
             dataframe=None,
-            model=SimpleNamespace()
-        )
-        self.assertEqual(
-            base.CleaningStrategy.can_use_cleaner(instance),
-            (False, "The model does not have a DataCleaner class."),
+            required_options=['option_1'],
         )
 
-    def test_validate_model_pass(self):
-        """Test that the `validate_model` method does not raise an error when
+        can_use, missing_opts = base.CleaningStrategy.can_use_cleaner(instance)
+        self.assertFalse(can_use, missing_opts)
+        self.assertEqual(len(missing_opts), 1)
+
+    def test_validate_options_pass(self):
+        """Test that the `validate_options` method does not raise an error when
         the model has the attributes the method expects.
         """
 
         instance = SimpleNamespace(
-            dataframe=None,
-            model=SimpleNamespace(DataCleaner=1),
-            can_use_cleaner=lambda: (True, None),
+            can_use_cleaner=lambda: (True, []),
         )
-        base.CleaningStrategy.validate_model(instance)
+        base.CleaningStrategy.validate_options(instance)
 
-    def test_validate_model_fail(self):
-        """Test that the `validate_model` method raises an error when the
+    def test_validate_options_fail(self):
+        """Test that the `validate_options` method raises an error when the
         model does not have the attributes the method expects.
         """
 
         instance = SimpleNamespace(
             dataframe=None,
             model=SimpleNamespace(),
-            can_use_cleaner=lambda: (False, "Failed"),
+            can_use_cleaner=lambda: (False, ["Failed"]),
         )
-        with self.assertRaises(NotImplementedError):
-            base.CleaningStrategy.validate_model(instance)
+        with self.assertRaises(exceptions.MissingOptionsError):
+            base.CleaningStrategy.validate_options(instance)
 
 
 class TestCleanData(TestCase):
@@ -74,29 +71,25 @@ class TestCleanData(TestCase):
         """
 
         # Create a test dataframe.
-        dataframe = pd.DataFrame(
-            {
-                "col1": [1, 2, 3],
-                "col2": [4, 5, 6],
-                "col3": [7, 8, 9],
-            }
-        )
+        dataframe = pd.DataFrame({
+            "col1": [1, 2, 3],
+            "col2": [4, 5, 6],
+            "col3": [7, 8, 9],
+        })
 
         # make a copy of the dataframe to test that the strategies are applied
         # in the correct order.
         results = base.clean_data(
             dataframe,
-            None,
-            [strategies.ReverseStrategy, strategies.FirstTwoRowsStrategy]
+            [strategies.ReverseStrategy, strategies.FirstXRowsStrategy],
+            x_top_rows=2
         ).reset_index(drop=True)
 
-        expected_results = pd.DataFrame(
-            {
-                "col1": [3, 2],
-                "col2": [6, 5],
-                "col3": [9, 8],
-            }
-        )
+        expected_results = pd.DataFrame({
+            "col1": [3, 2],
+            "col2": [6, 5],
+            "col3": [9, 8],
+        })
         self.assertTrue(
             results.equals(expected_results),
             f"\nActual:\n{results}\nExpected:\n{expected_results}",
@@ -104,14 +97,12 @@ class TestCleanData(TestCase):
 
     def test_no_mutations(self):
         """Test that the original dataframe is not mutated."""
-        dataframe = pd.DataFrame(
-            {
-                "col1": [1, 2, 3],
-                "col2": [4, 5, 6],
-                "col3": [7, 8, 9],
-            }
-        )
+        dataframe = pd.DataFrame({
+            "col1": [1, 2, 3],
+            "col2": [4, 5, 6],
+            "col3": [7, 8, 9],
+        })
 
         dataframe_copy = dataframe.copy()
-        base.clean_data(dataframe, None, [strategies.ReverseStrategy])
+        base.clean_data(dataframe, [strategies.ReverseStrategy])
         self.assertTrue(dataframe.equals(dataframe_copy))
---
+++ tests/test_strategies.py
@@ -0,0 +1,126 @@
+# """Unittests for the `methods` module."""
+
+# import unittest
+# import pandas as pd
+# from data_cleaners import methods
+# from tests.utils import models
+
+
+# class TestRemoveDuplicates(unittest.TestCase):
+#     """Unittests for the `RemoveDuplicates` class."""
+
+#     def test_clean_invalid_model(self):
+#         """Test that when a module is provided for which a field map has not
+#         been defined, an `ImplementationError` is raised.
+#         """
+#         remove_duplicates = methods.RemoveDuplicates(
+#             pd.DataFrame({"a": [1, 2, 3]}),
+#             models.MisconfiguredModel
+#         )
+#         with self.assertRaises(NotImplementedError):
+#             remove_duplicates.clean()
+
+#     def test_with_model(self):
+#         """Test that the data for a valid model is cleaned correctly."""
+#         dataframe = pd.DataFrame(
+#             {
+#                 "id": [1, 2, 1],
+#                 "name": ["a", "a", "a"],
+#                 "email": ['a@a.com', 'b@b.com', 'a@a.com'],
+#                 "age": [1, 2, 1],
+#                 "active": [True, True, False],  # Note: This is the only change
+#             }
+#         )
+#         remove_duplicates = methods.RemoveDuplicates(
+#             dataframe,
+#             models.User
+#         )
+#         remove_duplicates.clean()
+#         results = remove_duplicates.dataframe.reset_index(drop=True)
+
+#         expected_results = pd.DataFrame(
+#             {
+#                 "id": [2, 1],
+#                 "name": ["a", "a"],
+#                 "email": ['b@b.com', 'a@a.com'],
+#                 "age": [2, 1],
+#                 "active": [True, False]
+#             }
+#         )
+
+#         self.assertTrue(
+#             results.equals(expected_results),
+#             f"\nActual:\n{results}\nExpected:\n{expected_results}",
+#         )
+
+
+# class TestFilterValidForeignKeys(unittest.TestCase):
+#     """Unittests for the `FilterValidForeignKeys` class."""
+
+#     def test_ad_group(self):
+#         """Test that the `clean` removes the correct data when being used for
+#         the `campaign_models.AdGroup` class.
+#         """
+
+#         # Load the Campaign model with data as the AdGroup model contains a
+#         # foreign key to the Campaign model.
+#         dataframe = pd.DataFrame({
+#             "id": [1, 2, 3],
+#             "user_id": [1, 2, 3],
+#             "product_id": [1, 2, 3],
+#             "price": [1, 2, 3],
+#             "quantity": [1, 2, 3],
+#         })
+
+#         filter_valid_fks = methods.DjFilterValidForeignKeys(
+#             dataframe,
+#             models.Purchase
+#         )
+#         filter_valid_fks.clean()
+#         results = filter_valid_fks.dataframe.reset_index(drop=True)
+
+#         expected_results = pd.DataFrame({
+#             "id": [1, 2],
+#             "user_id": [1, 2],
+#             "product_id": [1, 2],
+#             "price": [1, 2],
+#             "quantity": [1, 2],
+#         })
+
+#         self.assertTrue(
+#             results.equals(expected_results),
+#             f"\nActual:\n{results}\nExpected:\n{expected_results}",
+#         )
+
+
+# # class RemoveColumns(unittest.TestCase):
+# #     """Unittests for the `RemoveColumns` class."""
+
+# #     def test_remove_columns(self):
+# #         """Test that the `clean` method removes the correct columns."""
+# #         dataframe = pd.DataFrame(
+# #             {
+# #                 "a": [1, 2, 3],
+# #                 "b": [1, 2, 3],
+# #                 "c": [1, 2, 3],
+# #             }
+# #         )
+
+# #         model = SimpleNamespace(
+# #             DataCleaner=SimpleNamespace(remove_columns=["a", "b"])
+# #         )
+
+# #         remove_columns = methods.RemoveColumns(dataframe, model)
+# #         remove_columns.clean()
+# #         results = remove_columns.dataframe.reset_index(drop=True)
+
+# #         expected_results = pd.DataFrame({"c": [1, 2, 3]})
+
+# #         self.assertTrue(
+# #             results.equals(expected_results),
+# #             f"\nActual:\n{results}\nExpected:\n{expected_results}",
+# #         )
+
+
+# # if __name__ == "__main__":
+# #     unittest.main()
